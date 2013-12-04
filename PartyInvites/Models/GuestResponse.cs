89115e900d299d0a7582783756cfbb7a512efe60
oaketree <oaketree@163.com>
2013-12-04 16:23:33 +0800

    12月4日下班

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage="请输入名字")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "请输入邮件地址")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
         public bool? WillAttend { get; set; }
 
     }
